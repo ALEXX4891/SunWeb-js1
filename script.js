829e72077afa0b6d1570ef0e8975f8337c79fbd0
ALEX4891 <alexx4891@mail.ru>
2023-12-01 17:46:01 +0500

    add code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const staffs = [
         salary: 5000,
         married: false,
         skills: ["html", "css", "js"],
-        employment_at: "2020-01-01"
+        employmentAt: "2020-01-01"
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ const staffs = [
         salary: 4000,
         married: true,
         skills: ["html", "css", "js", "php"],
-        employment_at: "2023-06-21"
+        employmentAt: "2023-06-21"
     },
     {
         id: 3,
@@ -27,7 +27,7 @@ const staffs = [
         salary: 6000,
         married: false,
         skills: ["html", "css", "js", "python"],
-        employment_at: "2021-03-15"
+        employmentAt: "2021-03-15"
     },
     {
         id: 4,
@@ -37,7 +37,7 @@ const staffs = [
         salary: 4500,
         married: true,
         skills: ["html", "css"],
-        employment_at: "2022-09-01"
+        employmentAt: "2022-09-01"
     },
     {
         id: 5,
@@ -47,7 +47,7 @@ const staffs = [
         salary: 7000,
         married: true,
         skills: ["html", "css", "js", "python", "java"],
-        employment_at: "2020-07-10"
+        employmentAt: "2020-07-10"
     },
     {
         id: 6,
@@ -57,7 +57,7 @@ const staffs = [
         salary: 5000,
         married: true,
         skills: ["js", "C++"],
-        employment_at: "2023-02-28"
+        employmentAt: "2023-02-28"
     },
     {
         id: 7,
@@ -67,7 +67,7 @@ const staffs = [
         salary: 5500,
         married: true,
         skills: ["html", "css", "js"],
-        employment_at: "2021-11-05"
+        employmentAt: "2021-11-05"
     },
     {
         id: 8,
@@ -77,31 +77,25 @@ const staffs = [
         salary: 4000,
         married: true,
         skills: ["html", "css", "js"],
-        employment_at: "2022-08-15"
+        employmentAt: "2022-08-15"
     }
 ]
 
 
 
+const copyStaffs = [...staffs];
+let staffsListForRender = [];
+// let arrayOfStaffs = [];
 
 
-let modal = document.getElementById('modal');
+const modal = document.getElementById('modal');
 
-// let modal = document.querySelector('#modal');
+const tableBody = document.getElementById('table-body');
+const form = document.querySelector('form');
+const saveBtn = document.querySelector('.save-modal');
 
-let form = document.querySelector('form')
-let saveBtn = document.querySelector('.save-modal')
-// let filter = document.getElementById('filter');
-// let tableBody = document.getElementById('table-body');
-// let male = document.getElementById('male');
-// let female = document.getElementById('female');
-// let staffName = document.getElementById('staff-name');
-// let staffDate = document.getElementById('staff-date');
-// let staffSalary = document.getElementById('staff-salary');
-// let staffAge = document.getElementById('staff-age');
-// let staffSkills = document.getElementById('staff-skills');
-let btnOpenModal = document.querySelector('.open-modal');
-let btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
 
 function modalToOpen() {
@@ -115,35 +109,157 @@ function modalToClose() {
 let formData;
 let data;
 
-function saveForm() {
-    formData = new FormData(form);
-    data = Object.fromEntries(formData.entries());
-    console.log(data)
-    modal.classList.remove('show');
-}
+// data:
+// age:"545"
+// employmentAt:"2023-12-02"
+// gender:"female"
+// name:"папап"
+// salary:"45454"
+// skills:"пвпвппавпв"
+
 
 saveBtn.addEventListener('click', saveForm);
 btnOpenModal.addEventListener('click', modalToOpen);
 btnCloseModal.addEventListener('click', modalToClose);
-// let formData = new FormData(form);
-// let data = Object.entries(formData.entries());
-// let save = e
+
+
+// получаем строку таблицы сотрудника и выводим её в таблицу:
+function getStaffItem(staffObj) {
+    const $tableRow = document.createElement("tr"),
+        $tableDataId = document.createElement("td"),
+        $tableDataName = document.createElement("td"),
+        $tableDataSkills = document.createElement("td"),
+        $tableDataDate = document.createElement("td"),
+        $tableDataGender = document.createElement("td"),
+        $tableDataAge = document.createElement("td"),
+        $tableDataSalary = document.createElement("td");
+  
+    $tableDataId.textContent = staffObj.id;
+    $tableDataName.textContent = staffObj.name;
+    $tableDataSkills.textContent = staffObj.skills;
+    $tableDataDate.textContent = staffObj.date;
+    $tableDataGender.textContent = staffObj.gender;
+    $tableDataAge.textContent = staffObj.age;
+    $tableDataSalary.textContent = staffObj.salary;
 
 
 
-// modal.onsubmit = async (e) => {
-//     e.preventDefault();
+    // $tableDataId
+    // $tableDataName
+    // $tableDataDate
+    // $tableDataGender
+    // $tableDataAge
+    // $tableDataSalary
+  
+    $tableRow.append($tableDataId);
+    $tableRow.append($tableDataName);
+    $tableRow.append($tableDataSkills);
+    $tableRow.append($tableDataDate);
+    $tableRow.append($tableDataGender);
+    $tableRow.append($tableDataAge);
+    $tableRow.append($tableDataSalary);
 
-//     let response = await fetch('#', {
-//       method: 'POST',
-//       body: new FormData(modal)
-//     });
-
-//     let result = await response.json();
-
-//     console.log(result.message);
-//   };
+    tableBody.append($tableRow);
+    // table.append(tableBody);
+}
 
 
+// for (const staffObj of copyStaffs) {
+//     getStaffItem(staffObj);
+// }
 
 
+// пререндер нужен нам для обработки и обработки даннныхЖ
+let staffsObjForRender = {};
+function preRender(staffObj) {
+    staffsObjForRender = {
+    id: staffObj.id,
+    name: staffObj.name,
+    age: +staffObj.age,
+    gender: staffObj.gender,
+    salary: +staffObj.salary,
+    skills: staffObj.skills,
+    date: staffObj.employmentAt,
+    // married: staffObj.married,
+    };
+}
+
+
+staffsListForRender = [];
+// запуск пререндера таблицы
+
+function preRenderTableOfStaff(arr) {
+    arr.forEach(item => {
+        preRender(item)
+        // staffsListForRender = []
+        staffsListForRender.push(staffsObjForRender);
+    });     
+}
+preRenderTableOfStaff(copyStaffs);
+
+
+// запуск рендерa:
+function renderTableOfStaff(arr) {
+    formData = new FormData(form);
+    preRenderTableOfStaff(copyStaffs)
+    arr.forEach(item => {
+        getStaffItem(item)
+    });     
+}
+
+renderTableOfStaff(staffsListForRender);
+
+
+
+function saveForm() {
+    data = Object.fromEntries(formData.entries());
+    console.log(data)
+    //присваиваем id новому сотруднику:
+    data.id = Math.max.apply(null, staffsListForRender.map(a => a.id)) + 1; 
+    // console.log(data)
+    data.date = data.employmentAt;
+    data.salary = +data.salary;
+    delete data.employmentAt;
+    modal.classList.remove('show');
+    // staffsListForRender.push(data);
+    // preRender(data);
+    staffsListForRender.push(data)
+    tableBody.innerHTML = '';
+    preRenderTableOfStaff(staffsListForRender)
+    renderTableOfStaff(staffsListForRender);
+
+    // for (const staffObj of staffsListForRender) {
+    //     getStaffItem(staffObj);
+    // }
+    // getStaffItem(staffsListForRender);
+    // console.log(staffsListForRender);
+}
+
+// let str = '';
+// function filterList(str) {
+//     staffsListForRender.filter((el) => el.some(str));
+
+//     for (const staffObj of staffsListForRender) {
+//         getStaffItem(staffObj);
+//     }
+    
+// }
+
+function filterTable(param, arr, str) {
+    return arr.filter((oneStaff) =>
+      oneStaff[param].toLowerCase().includes(str)
+    //   oneStaff[param].toLowerCase().includes(col.value.trim().toLowerCase())
+
+    );
+}
+
+let res = filterTable('name', staffsListForRender, 'jane');
+
+function sortTable(arr, property, sortDirection) {
+    arr.sort((a, b) =>
+    (sortDirection ? a[property] < b[property] : a[property] > b[property])
+      ? -1
+      : 1
+  );
+  renderStudentsTable(arr);
+}
