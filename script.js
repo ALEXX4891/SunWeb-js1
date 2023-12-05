f3033c9d99adb1df74070eba0b64b22830ff42cd
ALEX4891 <alexx4891@mail.ru>
2023-12-05 13:09:00 +0500

    выполнить второе задание - пагинация и связь с сервером

--- script.js
+++ script.js
@@ -1,160 +1,48 @@
-// const staffs = [
-//     {
-//         id: 1,
-//         name: "John",
-//         age: 30,
-//         gender: "male",
-//         salary: 5000,
-//         married: false,
-//         skills: ["html", "css", "js"],
-//         employmentAt: "2020-01-01"
-//     },
-//     {
-//         id: 2,
-//         name: "Jane",
-//         age: 25,
-//         gender: "female",
-//         salary: 4000,
-//         married: true,
-//         skills: ["html", "css", "js", "php"],
-//         employmentAt: "2023-06-21"
-//     },
-//     {
-//         id: 3,
-//         name: "Bob",
-//         age: 35,
-//         gender: "male",
-//         salary: 6000,
-//         married: false,
-//         skills: ["html", "css", "js", "python"],
-//         employmentAt: "2021-03-15"
-//     },
-//     {
-//         id: 4,
-//         name: "Alice",
-//         age: 28,
-//         gender: "female",
-//         salary: 4500,
-//         married: true,
-//         skills: ["html", "css"],
-//         employmentAt: "2022-09-01"
-//     },
-//     {
-//         id: 5,
-//         name: "Charlie",
-//         age: 40,
-//         gender: "male",
-//         salary: 7000,
-//         married: true,
-//         skills: ["html", "css", "js", "python", "java"],
-//         employmentAt: "2020-07-10"
-//     },
-//     {
-//         id: 6,
-//         name: "Emily",
-//         age: 32,
-//         gender: "female",
-//         salary: 5000,
-//         married: true,
-//         skills: ["js", "C++"],
-//         employmentAt: "2023-02-28"
-//     },
-//     {
-//         id: 7,
-//         name: "David",
-//         age: 29,
-//         gender: "male",
-//         salary: 5500,
-//         married: true,
-//         skills: ["html", "css", "js"],
-//         employmentAt: "2021-11-05"
-//     },
-//     {
-//         id: 8,
-//         name: "Sophia",
-//         age: 27,
-//         gender: "female",
-//         salary: 4000,
-//         married: true,
-//         skills: ["html", "css", "js"],
-//         employmentAt: "2022-08-15"
-//     },
-//     {
-//         id: 9,
-//         name: "Jane",
-//         age: 55,
-//         gender: "female",
-//         salary: 4500,
-//         married: false,
-//         skills: ["css", "js", "php"],
-//         employmentAt: "2020-05-22"
-//     }
-// ]
-
-
 const baseUrl = 'https://api.slingacademy.com/v1/sample-data';
 
-// let staffs = [];
 let responce;
 let staffsListForRender = [];
-let staffsList2;
+let usersFromDB;
 
-const staffsList = async function getStaffsFromDB(limit=10, offset=0) {
+const staffsList = async function getStaffsFromDB(limit=10, offset=0) {    
     const url = `/users?offset=${offset}&limit=${limit}`;
     responce = await fetch(baseUrl + url);
     let data = await responce.json();
-    // console.log(staffsList.users);
     if (!responce.ok) {
         console.log("Данные на сервере отсутсуют");
         return null;
     } 
-    console.log(data.users)
-    staffsList2 = [...data.users];
-    // console.log(staffsList2)
-    preRenderTableOfStaff(staffsList2);
-    console.log(staffsListForRender)
-    
+    staffsListForRender = [];
+    usersFromDB = [...data.users];
+    preRenderTableOfStaff(usersFromDB);
     renderTableOfStaff(staffsListForRender);
-
-
-    // return staffsListForRender;
-
-    // staffsList = [];
 }
 
-// await getStaffsFromDB();
-let limit = 10;
+let limit = 5;
 let offset = 0;
-// await getStaffsFromDB(limit, offset);
 staffsList(limit, offset);
 
-
-// if (staffsList) {
-//     staffsListForRender = [...staffsList];    
-//     console.log(staffsListForRender)
-// }
-
-const modal = document.getElementById('modal');
+// const modal = document.getElementById('modal');
 const filterInput = document.getElementById('filter');
 const tableBody = document.getElementById('table-body');
-const form = document.querySelector('form');
-const saveBtn = document.querySelector('.save-modal');
-const btnOpenModal = document.querySelector('.open-modal');
+// const form = document.querySelector('form');
+// const saveBtn = document.querySelector('.save-modal');
+// const btnOpenModal = document.querySelector('.open-modal');
 
-saveBtn.addEventListener('click', saveForm);
-btnOpenModal.addEventListener('click', modalToOpen);
+// saveBtn.addEventListener('click', saveForm);
+// btnOpenModal.addEventListener('click', modalToOpen);
 
 // находим кнопки закрытия модалки и навешиваем событие закрытия
-document.querySelectorAll('[data-bs-dismiss="modal"]')
-    .forEach(btn => { btn.addEventListener('click', modalToClose); });
+// document.querySelectorAll('[data-bs-dismiss="modal"]')
+//     .forEach(btn => { btn.addEventListener('click', modalToClose); });
 
-function modalToOpen() {
-    modal.classList.add('show');
-}
+// function modalToOpen() {
+//     modal.classList.add('show');
+// }
 
-function modalToClose() {
-    modal.classList.remove('show');
-}
+// function modalToClose() {
+//     modal.classList.remove('show');
+// }
 
 // получаем строку таблицы сотрудника и выводим её в таблицу:
 function getStaffItem(staffObj) {
@@ -211,6 +99,25 @@ function onDelete({ staffObj, element }) {
     element.remove();
     staffsListForRender = staffsListForRender.filter(x => x.id != staffObj.id);
     renderTableOfStaff(staffsListForRender);
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${staffObj.id}`, {
+        method: "DELETE",
+    })
+    .then((response) => {
+        if (response.ok) {
+            console.log(`статус запроса: ${response.status}`);
+            return response.json();
+
+        } else {
+            throw new Error(`Ошибка запроса: ${response.status}`);
+        }
+    })
+    .then(data => {
+        console.log(`Данные: ${data}`);
+    })
+    .catch(error => {
+        console.log(`Произошла ошибка: ${error}`)
+    })
 }
 
 // пререндер нужен нам для обработки и обработки даннныхЖ
@@ -220,7 +127,8 @@ function preRender(staffObj) {
     id: staffObj.id,
     first_name: staffObj.first_name,
     last_name: staffObj.last_name,
-    age: new Date().getFullYear() - new Date(staffObj.date_of_birth).getFullYear(),
+    age: staffObj.age ? staffObj.age : new Date().getFullYear() - new Date(staffObj.date_of_birth).getFullYear(),
+    // age: staffObj.age,
     gender: staffObj.gender,
     state: staffObj.state,
     street: staffObj.street,
@@ -238,26 +146,24 @@ function preRenderTableOfStaff(arr) {
 function renderTableOfStaff(arr) {
     tableBody.innerHTML = ""; // очищаем тело таблицы
     let copyList = [...arr]; // создаем копию массива
-
     if (filterInput.value.trim() !== "") {
         copyList = filterTable(filterInput, copyList);
     }
-
     copyList.forEach(staffObj => {
         getStaffItem(staffObj)
     });     
 }
 
 // Добавление строки с новым пользователем:
-function saveForm() {
-    tableBody.innerHTML = '';
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries());
-    //присваиваем id новому сотруднику:
-    data.id = Math.max.apply(null, staffsListForRender.map(a => a.id)) + 1; 
-    staffsListForRender.push(preRender(data));
-    renderTableOfStaff(staffsListForRender);
-}
+// function saveForm() {
+//     tableBody.innerHTML = '';
+//     const formData = new FormData(form);
+//     const data = Object.fromEntries(formData.entries());
+//     //присваиваем id новому сотруднику:
+//     data.id = Math.max.apply(null, staffsListForRender.map(a => a.id)) + 1; 
+//     staffsListForRender.push(preRender(data));
+//     renderTableOfStaff(staffsListForRender);
+// }
 
 // ФИЛЬТРАЦИЯ:
 // фильтрация массива сотрудников:
@@ -302,8 +208,49 @@ idSortTableBtn.addEventListener("click", () => {
 
 salarySortTableBtn.addEventListener("click", () => {
     sortDirection = !sortDirection;
-    sortArr(staffsListForRender, "salary", sortDirection);
+    sortArr(staffsListForRender, "last_name", sortDirection);
 });
+
+// ПАГИНАЦИЯ
+// создаем кнопки пагинации:
+let activeBtn;
+function createPageButtons() {
+    const totalPages = 10;
+    const paginationContainer = document.createElement('div');
+    const paginationDiv = document.body.appendChild(paginationContainer);
+    paginationContainer.classList.add('pagination', 'd-flex', 'justify-content-center');
+
+    // Add page buttons
+    for (let i = 0; i < totalPages; i++) {
+        const pageButton = document.createElement('button');
+        pageButton.classList.add('page-item', 'btn');
+        pageButton.textContent = i + 1;
+        pageButton.addEventListener('click', () => {
+        offset = i * limit;
+        activeBtn = i;
+        tableBody.innerHTML = ""; // очищаем тело таблицы
+        staffsList(limit, offset);
+        updateActiveButtonStates();
+        });
+
+        document.body.appendChild(paginationContainer);
+        paginationDiv.appendChild(pageButton);
+        }
+    }
+
+    // выделяем кнопку активной / не активной
+    function updateActiveButtonStates() {
+    let pageButtons = document.querySelectorAll('.page-item');
+    pageButtons.forEach((button, index) => {
+        if (index === activeBtn) {
+        button.classList.add('active');
+        } else {
+        button.classList.remove('active');
+        }
+    });
+}
+
+createPageButtons();
 
 
 
