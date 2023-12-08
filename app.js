e2e0c307f80213759c8082789a106b8a2d71f716
ALEX4891 <alexx4891@mail.ru>
2023-12-08 12:50:53 +0500

    выполнил четвертое задание Vue

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+// eslint-disable-next-line no-unused-vars, no-undef
 var app = new Vue({
     el: '#app',
     data: {
         modalShow: false,
-        users: {},
+        staffObj: {},
         filterInput: '',
         limit: 10,
         offset: 0,
-        // staffsList: [],
+        data: {},
         usersFromDB: [],
         staffsListForRender: [],
         sortDirection: true,
@@ -14,13 +15,9 @@ var app = new Vue({
         baseUrl: 'https://api.slingacademy.com/v1/sample-data',
         activeBtn:0,
         totalPages: 0,
-        // pageNumber: 1,        
-        
+        page: 0,   
     },
     computed: {
-        // addStuff() {
-        //     return this.staffsListForRender.push(this.users)
-        // },
         sortedList: function(){
             let sortParam = this.sortParam;
             if (sortParam) {
@@ -53,18 +50,12 @@ var app = new Vue({
 	},
 
     mounted() {
-            // this.staffsList();
-            // this.preRenderTableOfStaff(this.usersFromDB);
-            // this.renderTableOfStaff(this.staffsListForRender);
-            // this.createPageButtons();
-            this.updateActiveButtonStates();
     },
     methods: {
         async staffsList() {
             let url = `/users?offset=${this.offset}&limit=${this.limit}`;                 
             const responce = await fetch(this.baseUrl + url);
             let data = await responce.json();
-            // console.log(data)
             if (!responce.ok) {
                 console.log("Данные на сервере отсутсуют");
                 return null;
@@ -75,69 +66,138 @@ var app = new Vue({
             this.preRenderTableOfStaff(this.usersFromDB);
 
         },
-        // createPageButtons() {
-
-            // const paginationContainer = document.createElement('div');
-            // const paginationDiv = document.body.appendChild(paginationContainer);
-            // paginationContainer.classList.add('pagination', 'd-flex', 'justify-content-center');
+        deleteStaff(staffObj) {
+            if (!confirm(`Вы точно хотите удалить сотрудника ${staffObj.first_name} c ID № ${staffObj.id}?`)) {
+              return;
+            }
+            // element.remove();
+            this.staffsListForRender = this.staffsListForRender.filter(x => x.id != staffObj.id);
+            // renderTableOfStaff(staffsListForRender);
         
-            // Add page buttons
-            // for (let i = 0; i < this.totalPages; i++) {
-                // const pageButton = document.createElement('button');
-                // pageButton.classList.add('page-item', 'btn');
-                // pageButton.textContent = i + 1;
-                // pageButton.addEventListener('click', () => {
-                //     this.offset = i * this.limit;
-                //     this.activeBtn = i;
-                //     this.tableBody.innerHTML = ""; // очищаем тело таблицы
-                //     this.staffsList(this.limit, this.offset);
-                //     this.updateActiveButtonStates();
-                // });        
-                // document.body.appendChild(paginationContainer);
-                // paginationDiv.appendChild(pageButton);
-        // },           
+            fetch(`https://jsonplaceholder.typicode.com/posts/${staffObj.id}`, {
+                method: "DELETE",
+            })
+            .then((response) => {
+                if (response.ok) {
+                    console.log(`статус запроса: ${response.status}`);
+                    return response.json();
+        
+                } else {
+                    throw new Error(`Ошибка запроса: ${response.status}`);
+                }
+            })
+            .then(data => {
+                console.log(`Данные: ${data}`);
+            })
+            .catch(error => {
+                console.log(`Произошла ошибка: ${error}`)
+            })
+        },
+        saveForm() {
+            let form = document.querySelector('form');
+            let formData = new FormData(form);
+            this.data = Object.fromEntries(formData.entries());
+            this.data.id = Math.max.apply(null, this.staffsListForRender.map(a => a.id)) + 1; 
+            let res = 0;
+    
+            const validation = [this.validateFirstName, this.validateLastName, this.validateAge, this.validateDate, this.validateState, this.validateStreet, this.validateSkills];
+            validation.map(func => {
+                const isValid = func();
 
-        // выделяем кнопку активной / не активной
-        // updateActiveButtonStates() {
-        //     // let pageButtons = document.querySelectorAll('.page-item');
-        //     // pageButtons.forEach((button, index) => {
-        //     //     if (index === this.activeBtn) {
-        //     //         button.classList.add('active');
-        //     //     } else {
-        //     //         button.classList.remove('active');
-        //     //     }
-        //     // });
-        // },
+                res += isValid ? 0 : 1;
+            });
+        
+            if (res != 0) {
+                return
+            }
+         
+            this.staffsListForRender.push(this.preRender(this.data));
+        },
 
-        // saveForm() {
-            // const formData = new FormData(form);
-            // const data = Object.fromEntries(formData.entries());
-            // this.staffsListForRender = [...this.staffsListForRender, {
-            // }];
+        validateFirstName() {
+            let value = this.data.first_name;
+            const regEx = /^[a-zA-Zа-яА-ЯёЁ\s-]{1,11}$/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-first_name').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-first_name').classList.remove('is-invalid')
+            return regEx.test(value);
+        },  
+    
+        validateLastName() {
+            let value = this.data.last_name;
+            const regEx = /^[a-zA-Zа-яА-ЯёЁ\s-]{1,11}$/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-last_name').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-last_name').classList.remove('is-invalid')
+            return regEx.test(value);
+        },
+    
+        validateAge() {
+            let value = this.data.age;
+            const regEx = /^\d{1,2}$/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-age').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-age').classList.remove('is-invalid')
+            return regEx.test(value);
+        },
+    
+        validateDate() {
+            let value = this.data.employmentAt;
+            const regEx = /\d{1,4}.\d{1,2}.\d{1,2}/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-date').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-date').classList.remove('is-invalid')
+            return regEx.test(value);
+        },
+    
+        validateState() {
+            let value = this.data.state;
+            const regEx = /^[a-zA-Zа-яА-ЯёЁ\s-]{1,11}$/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-state').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-state').classList.remove('is-invalid')
+            return regEx.test(value);
+        },
+    
+        validateStreet() {
+            let value = this.data.street;
+            const regEx = /^[a-zA-Zа-яА-ЯёЁ\s-]{1,11}$/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-street').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-street').classList.remove('is-invalid')
+            return regEx.test(value);
+        },
+    
+        validateSkills() {
+            let value = this.data.skills;
+            const regEx = /^[a-zA-Zа-яА-ЯёЁ\s-]{1,11}$/;
+            if (!regEx.test(value)) {
+                document.querySelector('#staff-skills').classList.add('is-invalid')
+                return
+            }
+            document.querySelector('#staff-skills').classList.remove('is-invalid')
+            return regEx.test(value);
+        },
 
-        // },
-
-        // openModal() {
-        //     this.modalShow = true;
-        // },
         sortArr(arr) {
             return arr.sort((a, b) =>
               (this.sortDirection ? a[this.sortParam] < b[this.sortParam] : a[this.sortParam] > b[this.sortParam])
                 ? -1
                 : 1
             );
-            // renderTableOfStaff(arr);
         },
-        // renderTableOfStaff(arr) {
-        //     // this.tableBody.innerHTML = ""; // очищаем тело таблицы
-        //     let copyList = [...arr]; // создаем копию массива
-        //     if (this.filterInput.trim() !== "") {
-        //         copyList = this.filterTable(this.filterInput, copyList);
-        //     }
-        //     copyList.forEach(staffObj => {
-        //         this.getStaffItem(staffObj)
-        //     });     
-        // },
         preRenderTableOfStaff(arr) {
             return arr.forEach(item => {
                 this.staffsListForRender.push(this.preRender(item));
@@ -167,7 +227,6 @@ var app = new Vue({
             );
         },
         changePage(e) {
-            // console.log((e.target.textContent - 1) * this.limit);
             this.offset = (e.target.textContent - 1) * this.limit;
         }
 
@@ -176,13 +235,6 @@ var app = new Vue({
         offset: function () {
             this.staffsList()
         }
-
-        // эта функция запускается при любом изменении вопроса
-    //     filterInput: function () {
-    //         this.staffsList = [...this.staffsListForRender]
-    //     //   this.answer = 'Ожидаю, когда вы закончите печатать...'
-    //       this.filterTable(this.filterInput, this.staffsList)
-    //     }
     },
 
     
